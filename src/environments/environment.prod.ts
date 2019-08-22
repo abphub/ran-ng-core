fdd7c192792fb9832b5edee78783aeb2fc63db49
abphub <adu@ran.xyz>
2019-08-22 11:00:28 +0800

    初始提交

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,25 @@
+export const environment = {
+  production: true,
+  hmr: false,
+  application: {
+    name: 'Assets',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44369',
+    clientId: 'Assets_App',
+    dummyClientSecret: '1q2w3e*',
+    scope: 'Assets',
+    showDebugInformation: true,
+    oidc: false,
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44369',
+    },
+  },
+  localization: {
+    defaultResourceName: 'Assets',
+  },
+};
