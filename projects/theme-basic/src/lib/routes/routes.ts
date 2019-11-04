751df32e0e270620a343a58bdbe979135c884fdd
王鹏飞 <786395139@qq.com>
2019-11-04 17:23:07 +0800

    日常提交

--- projects/theme-basic/src/lib/routes/routes.ts
+++ projects/theme-basic/src/lib/routes/routes.ts
@@ -5,18 +5,37 @@ import { ABP, eLayoutType } from '@abp/ng.core';
  */
 export const THEME_BASIC_ROUTES = {
   routes: [{
-    name: '模板模块',
+    name: '::Menu:Theme',
     path: 'theme',
     order: 3,
     layout: eLayoutType.application,
     children: [{
-      name: '模板模块顶部菜单',
-      parentName: '',
+      name: '::Menu:Theme:Light',
+      parentName: '::Menu:Theme',
+      path: 'light',
       children: [
-        { path: 'sections', name: 'Site::Menu:Sections', order: 1, },
-        { path: 'fields', name: 'Site::Menu:Fields', order: 2, },
-        { path: 'entities', name: 'Site::Menu:Entities', order: 3 },
-        { path: 'categories', name: 'Site::Menu:Categories', order: 5 },
+        {
+          path: 'theme-one',
+          name: '::Menu:Theme:Light:One',
+          order: 1,
+          children: [
+            {
+              path: 'theme-one-two',
+              name: '::Menu:Theme:Light:One:Tow',
+            }
+          ]
+        },
+        { path: 'theme-two', name: '::Menu:Theme:Light:Two', order: 2, },
+        { path: 'theme-three', name: '::Menu:Theme:Light:Three', order: 3 },
+      ]
+    }, {
+      name: '::Menu:Theme:Dark',
+      parentName: '::Menu:Theme',
+      path: 'dark',
+      children: [
+        { path: 'theme-one', name: '::Menu:Theme:Dark:One', order: 1, },
+        { path: 'theme-two', name: '::Menu:Theme:Dark:Two', order: 2, },
+        { path: 'theme-three', name: '::Menu:Theme:Dark:Three', order: 3 },
       ]
     }]
   }] as ABP.FullRoute[]
