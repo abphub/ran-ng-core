207d7270af4c4f98558e6a1b89adba0224ef6471
王鹏飞 <786395139@qq.com>
2019-11-04 17:44:28 +0800

    日常提交

--- projects/theme-basic/src/lib/routes/routes.ts
+++ projects/theme-basic/src/lib/routes/routes.ts
@@ -14,19 +14,9 @@ export const THEME_BASIC_ROUTES = {
       parentName: '::Menu:Theme',
       path: 'light',
       children: [
-        {
-          path: 'theme-one',
-          name: '::Menu:Theme:Light:One',
-          order: 1,
-          children: [
-            {
-              path: 'theme-one-two',
-              name: '::Menu:Theme:Light:One:Tow',
-            }
-          ]
-        },
+        { path: 'theme-one', name: '::Menu:Theme:Light:One', order: 1, },
         { path: 'theme-two', name: '::Menu:Theme:Light:Two', order: 2, },
-        { path: 'theme-three', name: '::Menu:Theme:Light:Three', order: 3 },
+        { path: 'theme-three', name: '::Menu:Theme:Light:Three', order: 3 }
       ]
     }, {
       name: '::Menu:Theme:Dark',
