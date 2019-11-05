ca3b0bb5802b7d71fc688664286eef56581aba06
csch961207 <1107554233@qq.com>
2019-11-05 14:43:24 +0800

    日常提交

--- projects/theme-basic/src/lib/components/header/header-bar.component.ts
+++ projects/theme-basic/src/lib/components/header/header-bar.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'ran-app-header-bar',
-    templateUrl: './header-bar.component.html'
+    templateUrl: './header-bar.component.html',
+    styles: [
+        '.active {}',
+        '.active:hover {color: #1b9aee;}',
+        '.bar-button {padding: 0 !important; min-width: 55px !important;line-height: 30px !important;margin-right: 10px;}'
+    ]
 })
 export class AppHeaderBarComponent {
 
