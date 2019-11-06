07a504fe7e2fe976f668c8a687acb63287e8aec8
csch961207 <1107554233@qq.com>
2019-11-06 16:32:20 +0800

    日常提交

---
+++ projects/theme-basic/src/lib/components/page/page-foot.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'ran-page-foot',
+    templateUrl: './page-foot.component.html',
+    styleUrls: ['./page-foot.component.scss']
+})
+export class PageFootComponent {
+    constructor(
+
+    ) {
+    }
+}
