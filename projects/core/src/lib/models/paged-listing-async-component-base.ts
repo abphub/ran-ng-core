5f9eadacdfd21efc6db6a7ab47b9787f6e97e7bb
王鹏飞 <786395139@qq.com>
2019-11-07 10:06:24 +0800

    日常提交

--- projects/core/src/lib/models/paged-listing-async-component-base.ts
+++
@@ -1,7 +0,0 @@
-import { Observable } from 'rxjs';
-
-export abstract class PagedListingAsyncComponentBase<T> {
-    isLoading = false;
-    protected data$: Observable<T[]>;
-    protected totalCount$: Observable<number>;
-}
--- projects/theme-basic/src/lib/components/page/page-content.component.ts
+++ projects/theme-basic/src/lib/components/page/page-content.component.ts
@@ -1,5 +1,6 @@
 import { Component, ContentChild } from '@angular/core';
 import { PageSidebarComponent } from './page-sidebar.component';
+import { ModalService } from '@ran-ng/core';
 
 @Component({
     selector: 'ran-page-content',
@@ -8,4 +9,7 @@ import { PageSidebarComponent } from './page-sidebar.component';
 })
 export class PageContentComponent {
     @ContentChild(PageSidebarComponent, { static: true }) pageSidebar: PageSidebarComponent;
+    constructor(private modalService: ModalService) {
+        console.log(modalService);
+    }
 }
