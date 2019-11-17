dbcd7fb212579cd25afb6431c89538b2cd7b47be
王鹏飞 <786395139@qq.com>
2019-11-17 16:34:48 +0800

    日常提交

--- projects/theme-basic/src/lib/components/page/page-content.component.ts
+++ projects/theme-basic/src/lib/components/page/page-content.component.ts
@@ -3,12 +3,15 @@ import { PageSidebarComponent } from './page-sidebar.component';
 
 @Component({
     selector: 'ran-page-content',
-    templateUrl: './page-content.component.html'
+    templateUrl: './page-content.component.html',
+    styles: [
+        `flex:{display:flex;height:100%}`
+    ]
 })
 export class PageContentComponent {
     @ContentChild(PageSidebarComponent, { static: true }) pageSidebar: PageSidebarComponent;
 
-    @HostBinding('class.d-flex')
+    @HostBinding('class.flex')
     get pageSidebarComponent() {
         return this.pageSidebar;
     }
